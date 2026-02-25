98f2eb8db6b91fa410b4c75662b006d0e1d6b0d0
Jeevan s <aryajeevan68@gmail.com>
2026-02-25 21:08:21 +0530

    implimented sample transaction using mysql and python

---
+++ backend/code1.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user =os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE")
+    
+)
+
+cursor =connection.cursor()
+print("connected succesfull")
+
+#transaction
+try:
+    
+    query="INSERT INTO students(name,age) VALUES(%s,%s)"
+    val=("sudeep",39)
+    cursor.execute(query,val)
+    
+    
+    query="INSERT INTO students(name,age) VALUES(%s,%s)"
+    val=("sachin",27)
+    cursor.execute(query,val)
+    
+    connection.commit()
+    
+except Exception as e:
+   
+    connection.rollback()
+    print("error :" ,e)
+    print("failed to add data ")
+
+
+cursor.close()
+connection.close()
