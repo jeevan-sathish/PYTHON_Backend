c025f8f7575469c28724fdd64b2e480f142cdd5c
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-09 20:08:50 +0530

    msql connected succesfully in new joinsql file

---
+++ backend/joinSql.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+conn =mysql.connector.connect(
+    host =os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE")
+)
+
+cursor = conn.cursor()
+
+if conn.is_connected():
+    print("connected succesfully")
+    
+query ="SELECT * FROM bank"
+cursor.execute(query)
+row =cursor.fetchall()
+
+for r in row:
+    print(r)
+cursor.close()
+conn.close()
