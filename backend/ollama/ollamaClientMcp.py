060e294e5e8d81783ae165d84194470e5b3fa8f5
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-14 23:16:55 +0530

    mcp running succesfully with ollama

--- backend/ollama/ollamaClientMcp.py
+++ backend/ollama/ollamaClientMcp.py
@@ -2,17 +2,19 @@ from mcp import ClientSession,StdioServerParameters
 from mcp.client.stdio import stdio_client
 import asyncio
 
+
 async def main():
-    server =StdioServerParameters(
+    server=StdioServerParameters(
         command="python",
         args=["ollamaServerMcp.py"]
     )
+    
     async with stdio_client(server) as (read,write):
         async with ClientSession(read,write) as session:
             await session.initialize()
-            prompt=str(input("enter the prompt:"))
+            prompt =input("enter the prompt:")
             result = await session.call_tool("getResponse",{"prompt":prompt})
-            print("responce:", result.content[0].text)
+            print("response is:", result.content[0].text)
 
 
 asyncio.run(main())
--- backend/ollama/ollamaServerMcp.py
+++ backend/ollama/ollamaServerMcp.py
@@ -1,17 +1,17 @@
 from mcp.server.fastmcp import FastMCP
 import ollama
 
-mcp = FastMCP("my mcp server with ollama")
+mcp =FastMCP("my mcp ollama server")
 
 @mcp.tool()
-def getResponse(prompt: str) -> str:
-    response = ollama.chat(
+def getResponse(prompt:str)->str:
+    
+    response =ollama.chat(
         model="llama3",
         messages=[{"role":"user","content":prompt}]
-        
+       
     )
-    
     return response["message"]["content"]
 
-if __name__ == "__main__":
+if __name__=="__main__":
     mcp.run()
