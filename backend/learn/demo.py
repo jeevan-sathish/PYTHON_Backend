1c4b7b51c07bb94fa1fd288db939e513bb98a3c8
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-24 19:17:06 +0530

    list fundamentals

--- backend/learn/demo.py
+++ backend/learn/demo.py
@@ -1,1 +1,12 @@
-print('hello')
+#list pratiece questions
+num =[10,20,40,30,50]
+print(num)
+num.append(60)
+print(num)
+num.insert(1,70)
+print(num)
+num.remove(70)
+print(num)
+
+for i in num:
+    print(i)
