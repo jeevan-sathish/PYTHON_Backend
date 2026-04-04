2c809825cd9aee4b9afdfd7e6694810aa1640fcc
Jeevan s <sathishjeevan2004@gmail.com>
2026-04-04 22:23:07 +0530

    groq implimentation working fine

--- backend/learn/groqCChat.py
+++ backend/learn/groqCChat.py
@@ -9,7 +9,7 @@ client =Groq(api_key=os.getenv("GROQ_API_KEY"))
 response = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
     messages=[
-        {"role":"user","content":'what is rag in one word'}
+        {"role":"user","content":'what is Ai in one word'}
     ]
 )
 print(response.choices[0].message.content)
