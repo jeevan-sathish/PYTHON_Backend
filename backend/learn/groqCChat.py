06b224678393ec7524780016a519e57045ef660f
Jeevan s <sathishjeevan2004@gmail.com>
2026-04-04 22:22:29 +0530

    groq working fine

--- backend/learn/groqCChat.py
+++ backend/learn/groqCChat.py
@@ -4,4 +4,12 @@ import os
 
 load_dotenv()
 
-response =Groq
+client =Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {"role":"user","content":'what is rag in one word'}
+    ]
+)
+print(response.choices[0].message.content)
