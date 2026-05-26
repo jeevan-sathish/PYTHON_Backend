a56e66a2312c9ebb5b7c7398e2265d3cad689ff3
Jeevan s <sathishjeevan2004@gmail.com>
2026-05-26 23:26:07 +0530

    added some text

--- frontend/src/components/Feature.jsx
+++ frontend/src/components/Feature.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Feature = () => {
-  return <div>Feature</div>;
+  return (
+    <div>
+      F<h1>hello this is feature branch</h1>
+    </div>
+  );
 };
 
 export default Feature;
